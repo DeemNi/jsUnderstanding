eca19d5aafcf2b0101d4b1af4994ada1f9cdaad3
Nikita Demianov <hourlystory@gmail.com>
2025-04-22 05:29:16 +0300

    try to realize json.stringify не супер успішно, але core є

--- structures/array.js
+++ structures/array.js
@@ -1,10 +1,8 @@
-import { MyArray, shufleArray } from "./arrays/part_2.js";
-import { expandArr, expandArrStack, permute } from "./arrays/part_3.js";
-import { arrayFrom, bubleSort, checkTarget, insertionSort, map, mergeSort, quickSort, selectionSort, slidindWindow, slidindWindowIndex } from "./arrays/part_4.js";
+import { deepClone, find, slice, splice, stringify } from "./arrays/day_5.js";
 
 const dataArray = [1,2,3,4,[5,6,7],8,[1,[1,2,[1,2,[[1]]]],3]]
 
-const arr = [1, 4, 2, 10, 23, 3, 1, 0, 20]
+// let arr = [1, 4, 2, 10, 23, 3, 1, 0, 20]
 
 const obj = {
     k1: 111,
@@ -18,7 +16,15 @@ const obj = {
 
 // console.log(checkTarget(arr, 2))
 
-console.log(arrayFrom(obj, (a) => a))
+// console.log(arrayFrom(obj, (a) => a))
 
 // console.log(arr)
+
+const arr = [1,55,3,4,[1,2,3,4,[1,[2]]], {s:"12"}]
+console.log(stringify(arr))
+
+// console.log(arr)
+// console.log(splice(arr, 2, 1, '2',3,4))
+
+// console.log(find(arr, (el) => el > 2 ))
 
--- structures/arrays/day_5.js
+++ structures/arrays/day_5.js
@@ -1,12 +1,103 @@
 // Реалізуй slice() .Неглибоке копіювання масиву
 
+export function slice(arr, start = 0, end = arr.length){
+    const slicedArray = [];
+
+    const beginPosition = start < 0 ? Math.max(0, arr.length + start) : Math.min(start, arr.length);
+    const endPosition = end < 0 ? Math.max(0, arr.length + end) : Math.min(end, arr.length);
+
+    for(let i = beginPosition; i < endPosition; i++){
+        slicedArray.push(arr[i])
+    }
+
+    return slicedArray;
+}
+
 // Реалізуй splice() .Видалення/вставка елементів
+
+export function splice(arr, start = 0, delCount, ...items){
+    const splicedArray = [];
+
+    const beginPosition = start < 0 ? Math.max(0, arr.length + start) : Math.min(start, arr.length);
+    const deletedCount = delCount === undefined ? arr.length - beginPosition : Math.min(delCount, arr.length - beginPosition);
+
+    const before = [];  
+
+    for(let i = 0; i < beginPosition; i++){
+        before.push(arr[i])
+    }
+
+    const after = [];
+    
+    for(let i = beginPosition + deletedCount; i < arr.length; i++){
+        after.push(arr[i])
+    }
+
+    const deleted = []
+
+    for(let i = beginPosition; i < beginPosition + deletedCount; i++){
+        deleted.push(arr[i]);
+    }
+
+    arr.length = 0;
+    before.concat(...items, after).forEach(el => arr.push(el))
+
+    return deleted;
+}
 
 // Реалізуй Array.prototype.find() .Повернути перший, що задовольняє умову
 
+export function find(arr, callback, thisArg = this){
+    for(let i = 0; i < arr.length; i++){
+        if(callback.call(thisArg, arr[i], i, arr)) return arr[i];
+    }
+    return undefined
+}
+
 // Реалізуй deepClone() .Рекурсивне клонування об'єктів і масивів
 
+export function deepClone(arr){
+
+        if(Array.isArray(arr)){
+            return arr.map(el => deepClone(el))   
+        }else if(typeof arr === 'object' && arr !== null){
+            const newObj = {};
+
+            for(let key in arr){
+                newObj[key] = deepClone(arr[key])
+            }
+            return newObj
+        }else return arr
+
+}
+
 // Реалізуй JSON.stringify() (спрощено)	.Рекурсія + типи
+
+export function stringify(value, replacer, space){
+
+    if(typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean' || value === null){
+        return `"${value}"`;
+    }else if(Array.isArray(value)){
+       return `[${value.map(el => {
+        if(el === undefined || typeof el === 'function' || typeof el === 'symbol'){
+            return null
+        }
+        return stringify(el, replacer, space)
+       }).join(',')}]`
+    }else if(typeof value === 'object' && !Array.isArray(value)){
+        const props = Object.keys(value).reduce((acc, key) => {
+            const val = value[key];
+            if(typeof val === 'undefined' || typeof val === 'function' || typeof val === 'symbol'){
+                return acc
+            } 
+            acc.push(`"${key}":${stringify(val, replacer, space)}`)
+            return acc
+        }, []);
+
+        return `{${props.join(',')}}`;
+    }
+
+}
 
 // Binary Search .На відсортованих масивах
 
@@ -24,4 +115,4 @@
 
 // Maximum Subarray .Алгоритм Кадане
 
-// Search in Rotated Sorted Array Binary + логіка
+// Search in Rotated Sorted Array .Binary + логіка
